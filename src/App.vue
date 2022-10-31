449687f8ececc0baf9908b8e8119d3ee788c8c3b
1clone47 <91965237+1clone47@users.noreply.github.com>
2022-10-31 14:48:23 +0100

    Update HomeView.vue
    Changed `NavBar-Component`
    Added Content to `HomeView`

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-
 </script>
 
 <template>
   <header class="flex flex-row bg-stone-800 h-10 items-center space-x-4">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-14 h-7 text-white">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-    </svg>
-    <div class="flex flex-row items-center text-white">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-5 text-red-600">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-      </svg>
-      <p class="font-mono">YouTube</p>
-    </div>
-    <div class="flex flex-row  text-white text-center border ">
-      <input class="bg-black" type="text" placeholder="Suchen">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-5">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-      </svg>
+    <div class="flex flex-row">
+      <p class="text-white ml-4 text-lg">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </p>
+      <p class="text-lg text-white px-4 items-center">1clone47</p>
     </div>
   </header>
-
   <RouterView />
 </template>
-
--- src/views/MaintenanceView.vue
+++ src/views/MaintenanceView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex flex-row items-center mt-4">
-    <p class="flex-1 items-center text-center mt-8 border-2 ">Coming Soon</p>
+    <p class="flex-1 items-center">Coming Soon</p>
   </div>
 </template>
